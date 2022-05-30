f292f7e70432d31c180da5a2266041c38b0bfd4a
Parad1se <mihailov.tem@gmail.com>
2022-05-30 11:52:38 +0800

    add slider

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,12 @@
-console.log('Привет')
+
+$(document).ready(function(){
+    $('.slider__wrapper').slick({
+        accessibility: true,
+        // adaptiveHeight: true,
+        arrows: true,
+        speed: 1500,
+        prevArrow: '<button type="button" class="slick-prev"><img src="img/slider_arrow_left.png" alt="arrow"></button>',
+        nextArrow: '<button type="button" class="slick-next"><img src="img/slider_arrow_right.png" alt="arrow"></button>',
+    });
+  });
+      
